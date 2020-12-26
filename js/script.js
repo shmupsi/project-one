0c109891f6eb79f6ac2892ac70295019364246ec
annie <annmakarova98@gmail.com>
2020-12-26 20:18:17 +0300

    first commited

---
+++ js/script.js
@@ -0,0 +1,20 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+    menuItem = document.querySelectorAll('.menu_item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+        menu.style.transition='all .8s ease-in-out';
+        
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+            
+        })
+    })
+})
